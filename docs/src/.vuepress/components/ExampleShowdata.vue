5552202aacd491ccb458f885d49a4f16d6a506d8
thanawat <thanawat.k@global.ntt>
2020-11-19 15:47:22 +0700

    ExampleShowdata - Create

---
+++ docs/src/.vuepress/components/ExampleShowdata.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>brand: {{ brand }}</p>
+    <p>series: {{ series }}</p>
+    <p>price: {{ price }}</p>
+    <p>colors: {{ colors }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        brand: 'nikon',
+        series: 'Z',
+        price: 300,
+        colors: ['red', 'gree', 'yellow']
+    }
+  }
+}
+</script>
